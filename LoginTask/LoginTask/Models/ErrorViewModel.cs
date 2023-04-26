59a3c1cc3e67d29e87e05da55a5dca9701859c20
Tuan <tuanqxktk5@outlook.com>
2023-04-26 11:25:13 +0700

    Initial commit

---
+++ LoginTask/LoginTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
